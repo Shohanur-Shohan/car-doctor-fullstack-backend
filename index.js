dbd822057371d4ba5398d88e9a4207722df1c59b
Shohanur-Shohan <shohanurshohan734@gmail.com>
2024-05-09 00:43:47 +0600

    delete service added

--- index.js
+++ index.js
@@ -66,6 +66,15 @@ async function run() {
       res.send(result);
     });
 
+    //delete user specified booked servie
+    app.delete("/delete-booked/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookingCollection.deleteOne(query);
+      console.log(id, result);
+      res.send(result);
+    });
+
     app.listen(port, () => {
       console.log(`app is listening, ${port}`);
     });
